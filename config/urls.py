6efccf1ecb1c2a735239b58f04ab9eff6a3eb71a
Jon <jon.herrera.52929@ikasle.egibide.org>
2026-05-07 20:47:12 +0200

    fix: arreglo de rutas a diferentes secciones

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('inicio/', TemplateView.as_view(template_name='home.html'), name='inicio'),
     path('admin/', admin.site.urls),
     path('materiales/', include('materiales.urls')),
     path('usuarios/', include('usuarios.urls')),
